5e2136a9a1f5f7cdf12b747c795eeb350615171e
ibrohim <example@gmail.com>
2023-01-16 19:51:22 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// const name = prompt("chanel name");
+// console.log(`https://youtube.com/ ${name}`);
+
+// let incr = 10;
+// let decr = 10;
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+// console.log(2 - 2 * 2 !=8);
